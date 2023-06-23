52bf6974ab31e7a3eda710965a3b07e27067b7f7
AlessioAbbati <alex_117@live.it>
2023-06-23 17:23:46 +0200

    add cards

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,13 +1,6 @@
 <main>
     <div class="contents">
-        {{-- <app-comics
-          v-for="comic in arrComics"
-          :key="comic.series"
-          :urlImg="comic.thumb"
-          :price="comic.price"
-          :series="comic.series"
-        /> --}}
-
+        @include('partials.cards')
     </div>
     <button>LOAD MORE</button>
 </main>
---
+++ resources/views/partials/cards.blade.php
@@ -0,0 +1,9 @@
+<div class="cards">
+
+    {{-- <img :src="urlImg" :alt="series" />
+
+    <div class="text-container">
+        <h3>{{ series }}</h3>
+        <span>{{ price }}</span>
+    </div> --}}
+</div>
